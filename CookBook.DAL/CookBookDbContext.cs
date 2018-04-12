1ff528f61ae6e67221c4bde7fa0f4902155efd43
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-04-12 15:35:44 +0200

    Add CookBookDbContext.

---
+++ CookBook.DAL/CookBookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookBook.DAL.Entities;
+
+namespace CookBook.DAL
+{
+    public class CookBookDbContext : DbContext
+    {
+        public IDbSet<RecipeEntity> Recipes { get; set; }
+        public IDbSet<IngredientEntity> Ingredients { get; set; }
+    }
+}
