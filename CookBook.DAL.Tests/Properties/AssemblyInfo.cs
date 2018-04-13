cb7edd4179d5a523a9de1214275d2c904febd8b3
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-04-13 09:47:16 +0200

    CookBook.DAL.Tests

---
+++ CookBook.DAL.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CookBook.DAL.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CookBook.DAL.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ebc8cbce-67f9-4191-a286-8ae6bb079be9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CookBook.DAL.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace CookBook.DAL.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
