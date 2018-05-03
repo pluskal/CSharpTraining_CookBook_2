6e51ebcd3c1c67a470b20dc73ea15bfbd1c383fe
Jan Pluskal <jan.pluskal@gmail.com>
2018-05-03 10:59:29 +0200

    Add ViewModelBase

--- CookBook.UI.WPFApp/ViewModels/RecipeListViewModel.cs
+++ CookBook.UI.WPFApp/ViewModels/RecipeListViewModel.cs
@@ -1,23 +1,38 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Castle.Core.Internal;
 using CookBook.BL.Facades;
 using CookBook.BL.Facades.DTOs;
 
 namespace CookBook.UI.WPFApp.ViewModels
 {
-    public class RecipeListViewModel
+    public class RecipeListViewModel : ViewModelBase
     {
         private readonly RecipeFacade _recipeFacade;
+        private ObservableCollection<RecipeListDTO> _recipes;
 
         public RecipeListViewModel(RecipeFacade recipeFacade)
         {
             _recipeFacade = recipeFacade;
         }
 
-        public ObservableCollection<RecipeListDTO> Recipes { get; private set; }
-
-        private void OnLoad()
+        public ObservableCollection<RecipeListDTO> Recipes
         {
-            Recipes = new ObservableCollection<RecipeListDTO>(_recipeFacade.GetList());
+            get => _recipes;
+            private set
+            {
+                if (Equals(value, _recipes)) return;
+                _recipes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected override void OnLoad()
+        {
+            if (Recipes.IsNullOrEmpty())
+            {
+                Recipes = new ObservableCollection<RecipeListDTO>(_recipeFacade.GetList());
+            }
         }
     }
 }
---
+++ CookBook.UI.WPFApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CookBook.UI.WPFApp.Annotations;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace CookBook.UI.WPFApp.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        protected ViewModelBase()
+        {
+            LoadedCommand = new ActionCommand(OnLoad);
+        }
+
+        public ICommand LoadedCommand { get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnLoad()
+        {
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
